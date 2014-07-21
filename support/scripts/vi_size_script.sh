42314d93fd2249298568a015a40711db79c72216
EwanC <ewan.cr@gmail.com>
2014-07-21 14:07:28 +0100

    safety check for vim

--- support/scripts/vi_size_script.sh
+++ support/scripts/vi_size_script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ -x "vim" ]; then
+if [ -x "/usr/bin/vim" ]; then
    vim /tmp/output.cl -c " %s/int atomic_inc(__global int\*);// | wq "
 
 else 
--- support/scripts/vi_trace_script.sh
+++ support/scripts/vi_trace_script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ -x "vim" ]; then
+if [ -x "/usr/bin/vim" ]; then
   vim /tmp/output.cl -c "%s/convert_long// | %s/int atomic_inc(__global int\*);// | wq "
 else
   echo "Please install vim"
